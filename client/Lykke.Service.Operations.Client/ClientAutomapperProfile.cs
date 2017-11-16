c9ada63c906589ca6da998ce4170f85165daa674
Mikhail Musin <mikhail.musin@gmail.com>
2017-11-16 20:34:35 +0300

    LWDEV-4002 automapper client profile

---
+++ client/Lykke.Service.Operations.Client/ClientAutomapperProfile.cs
@@ -0,0 +1,16 @@
+﻿using AutoMapper;
+using Lykke.Contracts.Operations;
+using Lykke.Service.Operations.Contracts;
+
+namespace Lykke.Service.Operations.Client
+{
+    public class ClientAutomapperProfile : Profile
+    {
+        public ClientAutomapperProfile()
+        {
+            CreateMap<AutorestClient.Models.OperationModel, OperationModel>();
+            CreateMap<OperationStatus, AutorestClient.Models.OperationStatus>();
+            CreateMap<CreateTransferCommand, AutorestClient.Models.CreateTransferCommand>();
+        }
+    }
+}
--- client/Lykke.Service.Operations.Client/OperationsClient.cs
+++ client/Lykke.Service.Operations.Client/OperationsClient.cs
@@ -14,26 +14,17 @@ namespace Lykke.Service.Operations.Client
     public class OperationsClient : IOperationsClient, IDisposable
     {        
         private OperationsAPI _operationsApi;
-        private readonly IMapper _mapper;
-
+        
         public OperationsClient(string serviceUrl)
         {            
-            _operationsApi = new OperationsAPI(new Uri(serviceUrl));
-
-            var config = new MapperConfiguration(cfg =>
-            {
-                cfg.CreateMap<AutorestClient.Models.OperationModel, OperationModel>();
-                cfg.CreateMap<OperationStatus, AutorestClient.Models.OperationStatus>();
-                cfg.CreateMap<CreateTransferCommand, AutorestClient.Models.CreateTransferCommand>();
-            });            
-            _mapper = config.CreateMapper();
+            _operationsApi = new OperationsAPI(new Uri(serviceUrl));            
         }
 
         public async Task<OperationModel> Get(Guid id)
         {
             var op = await _operationsApi.ApiOperationsByIdGetAsync(id);
 
-            var result =  _mapper.Map<OperationModel>(op);
+            var result =  Mapper.Map<OperationModel>(op);
             result.Context = JObject.FromObject(op.Context);
 
             return result;
@@ -41,12 +32,12 @@ namespace Lykke.Service.Operations.Client
 
         public async Task<IEnumerable<OperationModel>> Get(Guid clientId, OperationStatus status)
         {
-            return (await _operationsApi.ApiOperationsByClientIdListByStatusGetAsync(clientId, _mapper.Map<AutorestClient.Models.OperationStatus>(status))).Select(m => _mapper.Map<OperationModel>(m));
+            return (await _operationsApi.ApiOperationsByClientIdListByStatusGetAsync(clientId, Mapper.Map<AutorestClient.Models.OperationStatus>(status))).Select(m => Mapper.Map<OperationModel>(m));
         }
 
         public async Task<Guid> Transfer(Guid id, CreateTransferCommand transferCommand)
         {
-            return (await _operationsApi.ApiOperationsTransferByIdPostAsync(id, _mapper.Map<AutorestClient.Models.CreateTransferCommand>(transferCommand))).Value;
+            return (await _operationsApi.ApiOperationsTransferByIdPostAsync(id, Mapper.Map<AutorestClient.Models.CreateTransferCommand>(transferCommand))).Value;
         }
 
         public async Task Cancel(Guid id)
