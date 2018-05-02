537d65b66962f71c8d379e7c8ce6ada3e4056e0a
Roman Chuprikov <paradise.develop@gmail.com>
2018-05-02 15:56:08 +0500

    remove unused DB connection string

--- src/Lykke.Service.Operations/Settings/ServiceSettings/DbSettings.cs
+++ src/Lykke.Service.Operations/Settings/ServiceSettings/DbSettings.cs
@@ -6,9 +6,9 @@ namespace Lykke.Service.Operations.Settings.ServiceSettings
     {
         [AzureTableCheck]
         public string LogsConnString { get; set; }
-        [AzureTableCheck]
-        public string OffchainConnString { get; set; }
+
         public string MongoConnectionString { get; set; }
+
         [AzureTableCheck]
         public string HMarketOrdersConnString { get; set; }
     }
