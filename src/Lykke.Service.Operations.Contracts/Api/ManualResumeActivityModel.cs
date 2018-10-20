06c0e6b167b3bb110ae07a9cd9b21ca53cea39cf
Roman Chuprikov <paradise.develop@gmail.com>
2018-10-20 15:14:03 +0500

    LWDEV-9947: manual resolving

---
+++ src/Lykke.Service.Operations.Contracts/Api/ManualResumeActivityModel.cs
@@ -0,0 +1,10 @@
+﻿namespace Lykke.Service.Operations.Contracts.Api
+{
+    /// <summary>
+    /// Model for manual activity resuming
+    /// </summary>
+    public class ManualResumeActivityModel
+    {
+        public string OutputJson { get; set; }
+    }
+}
