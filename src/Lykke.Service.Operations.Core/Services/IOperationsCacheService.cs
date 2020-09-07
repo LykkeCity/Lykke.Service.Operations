25e8c6864b0ced6a71b389c65dabcd825f95407b
Dmitry Samodov <samodov.dmitry@gmail.com>
2020-09-07 16:54:21 +0300

    LWDEV-10744: added operations cache service

---
+++ src/Lykke.Service.Operations.Core/Services/IOperationsCacheService.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Lykke.Service.Operations.Contracts;
+using Lykke.Service.Operations.Core.Domain;
+
+namespace Lykke.Service.Operations.Core.Services
+{
+    public interface IOperationsCacheService
+    {
+        Task<Operation> GetAsync(Guid id);
+        Task<IEnumerable<Operation>> GetAsync(Guid clientId, OperationStatus? status, OperationType? type, int? skip = 0, int? take = 10);
+        Task CreateAsync(Operation operation);
+        Task UpdateStatusAsync(Guid id, OperationStatus status);
+        Task SaveAsync(Operation operation);
+        Task ClearAsync();
+    }
+}
---
+++ src/Lykke.Service.Operations/PeriodicalHandlers/OperationsHandler.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using Autofac;
+using Common;
+using Common.Log;
+using JetBrains.Annotations;
+using Lykke.Common.Log;
+using Lykke.Service.Operations.Core.Services;
+
+namespace Lykke.Service.Operations.PeriodicalHandlers
+{
+    [UsedImplicitly]
+    public class OperationsHandler : IStartable, IStopable
+    {
+        private readonly IOperationsCacheService _operationsCacheService;
+        private readonly TimerTrigger _timerTrigger;
+        private readonly ILog _log;
+
+        public OperationsHandler(
+            IOperationsCacheService operationsCacheService,
+            ILogFactory logFactory
+            )
+        {
+            _operationsCacheService = operationsCacheService;
+            _log = logFactory.CreateLog(this);
+            _timerTrigger = new TimerTrigger(nameof(OperationsHandler), TimeSpan.FromHours(1), logFactory);
+            _timerTrigger.Triggered += Execute;
+        }
+
+        public void Start()
+        {
+            _timerTrigger.Start();
+        }
+
+        public void Stop()
+        {
+            _timerTrigger.Stop();
+        }
+
+        public void Dispose()
+        {
+            _timerTrigger.Stop();
+            _timerTrigger.Dispose();
+        }
+
+        private Task Execute(ITimerTrigger timer, TimerTriggeredHandlerArgs args, CancellationToken cancellationToken)
+        {
+            return _operationsCacheService.ClearAsync();
+        }
+    }
+}
